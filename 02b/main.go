cda8a066d330c26dedc4b0b66fbc1af2fb7a883b
Steph Janssen <steph.janssen@on2it.net>
2021-12-02 07:15:13 +0100

    2b works :)

---
+++ 02b/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type loc struct { // The location of the submarine
+	dep int // The depth (bigger is deeper!)
+	hor int // The horizontal position
+	aim int // The aim amount
+}
+
+func main() {
+	// lines, _ := readLines("example.txt") // lines contains the example input []string
+	lines, _ := readLines("input.txt") // lines contains the challenge input []string
+	_ = lines
+	Loc := ResolveLocation(lines)
+	fmt.Printf("The location of the sub is: %d,%d.\nMultiplied: %d", Loc.hor, Loc.dep, Loc.hor*Loc.dep)
+}
+
+func ResolveLocation(input []string) (Loc loc) {
+	for _, line := range input {
+		Loc = DoAction(line, Loc)
+	}
+	return Loc
+}
+
+func DoAction(line string, Loc loc) loc {
+	instructions := strings.Fields(line)
+	inc, err := strconv.Atoi(instructions[1])
+	if err != nil {
+		panic(err)
+	}
+	switch instructions[0] {
+	case "forward":
+		Loc.hor += inc
+		Loc.dep += inc * Loc.aim
+	case "down": // Increases aim by x units.
+		Loc.aim += inc
+	case "up": // Increases aim by x units.
+		Loc.aim -= inc
+	default:
+		panic("Gotten a strange input!" + instructions[0])
+	}
+	return Loc
+}
